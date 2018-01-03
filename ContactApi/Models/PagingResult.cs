fb1f3cd3cc577a0ed84645687068e5609d2c334b
afwanrapidd <mohammed.afwan@rapidd.net>
2018-01-03 11:24:22 +0530

    c#

---
+++ ContactApi/Models/PagingResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using ContactApi.Dto;
+
+namespace ContactApi.Models
+{
+    public class PagingResult
+    {
+        public IEnumerable<ContactDto> Data { get; set; }
+
+        public int Total { get; set; }
+    }
+}
